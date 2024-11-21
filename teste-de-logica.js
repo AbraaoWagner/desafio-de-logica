e3c26a320f87b8ea28b5d81ecd7f80ecd31b9836
AbraaoWagner <abraogamer60@gmail.com>
2024-11-21 03:00:59 -0300

    terceiro commit

--- teste-de-logica.js
+++ teste-de-logica.js
@@ -1,7 +1,23 @@
-console.log("Wello World!")
+let nickName = "Karenyou"
+let xpPlayer = 3000
+let rankPlayer = ""
 
-let teste = true
-
-if (teste) {
-    console.log("Funcionou!!!!!")
+if (xpPlayer <= 1000) {
+    rankPlayer = "Ferro"
+}else if(xpPlayer>=1001 && xpPlayer<=2000 ){
+    rankPlayer = "Bronze"
+}else if(xpPlayer>=2001 && xpPlayer<=5000 ){
+    rankPlayer = "Prata"
+}else if(xpPlayer>=5001 && xpPlayer<=7000 ){
+    rankPlayer = "Ouro"
+}else if(xpPlayer>=7001 && xpPlayer<=8000 ){
+    rankPlayer = "Platina"
+}else if(xpPlayer>=8001 && xpPlayer<=9000 ){
+    rankPlayer = "Ascendente"
+}else if(xpPlayer>=9001 && xpPlayer<=10000 ){
+    rankPlayer = "Imortal"
+}else {
+  rankPlayer = "Radiante"
 }
+
+console.log("O Herói "+nickName+" está no nível de "+rankPlayer)
